e72f174073ca8ec18b4dbf9a4f59d46d446ccab9
Vaibhav Mehandiratta <34084111+vaibhavsd3474@users.noreply.github.com>
2018-02-16 22:16:52 +0530

    Update W6D5StudyHallErrorTermRecognizer.java

--- W6D5StudyHallErrorTermRecognizer.java
+++ W6D5StudyHallErrorTermRecognizer.java
@@ -5,18 +5,17 @@ import java.util.Scanner;
 public class W6D5StudyHallExemplar {
 	/**
 	 * <p> Title: W6D5StudyHallErrorTerm. </p>
-	 * 
-	 * ErrorTerm Recognizer
+	 *
+	 * Error Term Recognizer
 	 * 
 	 * <p> Description: A demonstration of the mechanical translation of Finite State Machine 
 	 * diagram into an executable Java program using the error term recognizer. </p>
 	 * 
-	 * <p> Copyright: Lynn Robert Carter © 2018 </p>
+	 * <p> Copyright: Lynn Robert Carter Â© 2018 </p>
 	 * 
-	 * @author Lynn Robert Carter (Baseline)
+	 * @author Lynn Robert Carter
 	 * @author Vaibhav Mehandiratta
-	 * 
-	 * @version 0.02	2018-02-16	Changes in Baseline 
+	 * @version 0.01	2018-01-28	Changes in Initial Baseline 
 	 * 
 	 */
 	
@@ -43,18 +42,20 @@ public class W6D5StudyHallExemplar {
 	 * This private method indicates if a state is final for the purposes of writing out the trace
 	 * 
 	 * @param state		The state number whose final state is being questioned
-	 * @return			True if it is a final state, otherwise, false
+	 * @return			True if it is a final state, otherwise, false 
 	 */
 	private static boolean isFinalState(int state) {
 		switch (state) {
 		case 0: return false;
 		case 1: return false;
-		case 2: return true;
+		case 2: return false;
 		case 3: return false;
 		case 4: return true;
 		case 5: return false;
 		case 6: return false;
 		case 7: return true;
+		case 8: return false;
+		case 9: return false;
 		}
 		return false;
 	}
@@ -93,20 +94,16 @@ public class W6D5StudyHallExemplar {
 				// State 0 has three valid transitions.  Each is addressed by an if statement.
 				
 				// If the current character is in the range from 1 to 9, it transitions to state 1
-				if (currentChar >= '1' && currentChar <= '9') {
-					state = 2;
-					break;
-				}
-				// If the current character is a decimal point, it transitions to state 3
-				else if (currentChar == '.') {
-					state = 3;
-					break;					
-				}
-				// If the current character is a zero, it transitions to state 8
-				else if (currentChar == '0') {
+				if (currentChar >= '0' && currentChar <= '9') {
 					state = 1;
 					break;
 				}
+				// If the current character is a decimal point, it transitions to state 2
+				else if (currentChar == '.') {
+					state = 2;
+					break;					
+				}
+				
 				
 				// If it is none of those characters, the FSM halts
 				else 
@@ -116,76 +113,76 @@ public class W6D5StudyHallExemplar {
 				break;
 			
 			case 1: 
-				if (currentChar >= '1' && currentChar <= '9') {
-				    state = 2;
-					break;
-				}
-				else if (currentChar == '.') {
-					state = 3;
-					break;					
-				} 
-				else if (currentChar == '0') {
+                // State 0 has three valid transitions.  Each is addressed by an if statement.
+				
+				// If the current character is in the range from 1 to 9, it transitions to state 1
+				if (currentChar >= '0' && currentChar <= '9') {
 					state = 1;
 					break;
 				}
+				// If the current character is a decimal point, it transitions to state 3
+				else if (currentChar == '.') {
+					state = 3;
+					break;					
+				}
+				
+				
 				// If it is none of those characters, the FSM halts
 				else 
 					running = false;
-				
-				// The execution of this state is finished
 				break;
 			
 			case 2: 
+                // State 2 has three valid transitions.  Each is addressed by an if statement.
+				
+				// If the current character is in the range from 1 to 9, it transitions to state 1
 				if (currentChar >= '0' && currentChar <= '9') {
-				    state = 2;
+					state = 4;
 					break;
 				}
 				
-				else if(currentChar=='e'|| currentChar=='E') {
-					state = 5;
-				    break;
-				}
 				
-				else if (currentChar =='.') {
-					state = 3;
-					break;
-				}
-				
+				// If it is none of those characters, the FSM halts
 				else 
 					running = false;
 				break;
 			
 			case 3:
-				if (currentChar =='0') {
-					state = 3;
+                // State 0 has three valid transitions.  Each is addressed by an if statement.
+				
+				// If the current character is in the range from 1 to 9, it transitions to state 1
+				if (currentChar >= '0' && currentChar <= '9') {
+					state = 4;
 					break;
 				}
 				
-				else if (currentChar >= '1' && currentChar <= '9') {
-				    state = 4;
-					break;
-				}
 				
-				else if(currentChar=='e'|| currentChar=='E') {
-					state = 5;
-				    break;
-				}
 				
+				// If it is none of those characters, the FSM halts
 				else 
 					running = false;
 				break;
 			
 			case 4: 
-				if(currentChar=='e'|| currentChar=='E') {
+                // State 0 has three valid transitions.  Each is addressed by an if statement.
+				
+				// If the current character is in the range from 1 to 9, it transitions to state 1
+				if (currentChar >= '0' && currentChar <= '9') {
+					state = 4;
+					break;
+				}
+				// If the current character is a decimal point, it transitions to state 2
+				else if (currentChar == 'e') {
 					state = 5;
-				    break;
+					break;					
 				}
 				
+				
+				// If it is none of those characters, the FSM halts
 				else 
 					running = false;
-				
 				break;
-				
+			
 			case 5: 
 				// State 5 has two valid transitions. It is address by the following if statements.
 				
@@ -208,7 +205,7 @@ public class W6D5StudyHallExemplar {
 				
 				// The execution of this state is finished
 				break;
-				
+			
 			case 6: 
 				// State 6 has one valid transition. It is address by the following if statement.
 				
@@ -224,22 +221,59 @@ public class W6D5StudyHallExemplar {
 				
 				// The execution of this state is finished
 				break;
-				
+		
 			case 7: 
 				// State 7 has one valid transition. It is address by the following if statement.
 				
 				// If the current character is in the range from 0 through 9, it accepts that 
 				// character and it stays in state 7		
-				if (currentChar >= '0' && currentChar <= '9') {
-					state = 7;
+				if (currentChar >= '0' && currentChar <= '9')
 					break;
-				}
+				
 				// If it is none of those characters, the FSM halts
 				else
 					running = false;
 				
 				// The execution of this state is finished
 				break;
+			
+			case 8:
+				// State 8 has one valid transition. It is address by the following if statement.
+				
+				// If the current character is a decimal point, it accepts that character and it
+				// transitions to state 9		
+				if (currentChar == '.') {
+					state = 9;
+					break;
+				}
+				// If it is not that character, the FSM halts
+				else
+					running = false;
+				
+				// The execution of this state is finished
+				break;
+			
+			case 9: 
+				// State 9 has two valid transitions. It is address by the following if statements.
+				
+				// If the current character is in the range from 1 through 9, it accepts that 
+				// character and transition to state 4		
+				if (currentChar >= '1' && currentChar <= '9') {
+					state = 4;
+					break;
+				}
+				// if the current character is a zero, it accepts that character and stays in that
+				// state
+				else if (currentChar == '0') 
+					break;
+				
+				// If it is not that character, the FSM halts
+				else
+					running = false;
+				
+				// The execution of this state is finished
+				break;
+
 			}
 			
 			// Display the rest of the FSM trace line
@@ -251,7 +285,7 @@ public class W6D5StudyHallExemplar {
 					System.out.println("  " + currentChar);
 				else
 					System.out.println("None");
-			
+
 			// When the processing of a state has finished, the FSM proceeds to the next character
 			// in the input and if there is one, it fetches that character and updates the 
 			// currentChar.  If there is no next character the currentChar is set to a blank.
@@ -268,32 +302,27 @@ public class W6D5StudyHallExemplar {
 		// of the current state of the FSM and whether or not the whole string has been consumed.
 		// This switch directs the execution to separate code for each of the FSM states.
 		switch (state) {
-		
 		case 0:
 			// State 0 is not a final state, so we can return a very specific error message
-			return "The first character must be a digit or a decimal point.\n"+ 
-					displayInput(input, currentCharNdx);
-
+			return "The first character must be a digit or a decimal point.";
 		case 1:
-			// This is a stub that must be filled in
-			return "Character must be a non zero value.\n"+ 
-					displayInput(input, currentCharNdx);
+			//State 1 is not a final state, so we can return a very specific error message
+			return "Please enter a valid input string with a proper floating point number";
 
 		case 2:
 			// This is a stub that must be filled in
-			return "Character must be in between 0-9.\n"+ 
-					displayInput(input, currentCharNdx);
+			return "Please enter a number after the decimal point.";
 
 		case 3:
-			// This is a stub that must be filled in
-			return "There must be some other non zero values.\n"+ 
-					displayInput(input, currentCharNdx);
+			// State 3 is not a final state, so we can return a very specific error message
+			return "Please enter a number after the decimal point.";
 
 		case 4:
-			// This is a stub that must be filled in
-			return "Error term with single digit of precision is only allowed.\n"+ 
-					displayInput(input, currentCharNdx);
-
+			// State 4 is a final state, so we can return a specific success message.
+			if (currentCharNdx<=input.length())
+				return "This character may only be a digit\n" + displayInput(input, currentCharNdx);
+			else 
+				return "";
 		case 5:
 			// State 5 is not a final state.  In order to move forward, the next character must be
 			// a digit or a plus or a minus character.
@@ -315,7 +344,17 @@ public class W6D5StudyHallExemplar {
 			else
 				return "";			
 			
-				}
+		case 8:
+			// State 8 is not a final state.  In order to move forward, the next character must be
+			// a decimal point.
+			return "This character may only be a decimal point\n" + 
+				displayInput(input, currentCharNdx);	
+			
+		case 9:
+			// States 3, 6, 7, and 9 are the same. None of them are final states and in order to
+			// move forward, the next character must be a digit.
+			return "This character may only be a digit\n" + displayInput(input, currentCharNdx);
+		}
 		
 		// This statement is not needed because all possible paths above contain a return.  Since 
 		// the Java compiler can't know that, we must put a statement here to ensure that a valid
